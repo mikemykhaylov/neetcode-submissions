a5ce58d49dbbf35411a59de37edcb3e5fc9b8bd5
Michael Mykhaylov <32168861+mikemykhaylov@users.noreply.github.com>
2026-07-05 23:07:57 -0400

    Add: unique-email-addresses - submission-2

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-2.py
@@ -0,0 +1,13 @@
+def cleanmail(email: str) -> str:
+    local, domain = email.split("@")
+    local = local.split('+')[0].replace('.', '')
+    return "@".join([local, domain])
+
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        out = set()
+        for email in emails:
+            clean = cleanmail(email)
+            out.add(clean)
+
+        return len(out)
