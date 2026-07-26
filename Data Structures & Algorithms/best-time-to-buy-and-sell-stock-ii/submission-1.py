fe7cf80f460489a0eb8f83ed8517b8472545bcc0
Michael Mykhaylov <32168861+mikemykhaylov@users.noreply.github.com>
2026-07-26 15:31:35 -0400

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        curr = 0
+        profit = 0
+        buy = 0
+        sell = 1
+        while sell < len(prices):
+            if prices[sell] < prices[buy] or prices[sell] < prices[sell - 1]:
+                profit += curr
+                buy = sell
+                sell += 1
+                curr = 0
+                continue
+            
+            curr = max(curr, prices[sell] - prices[buy])
+            sell += 1
+
+        profit += curr
+        return profit
+
+        
